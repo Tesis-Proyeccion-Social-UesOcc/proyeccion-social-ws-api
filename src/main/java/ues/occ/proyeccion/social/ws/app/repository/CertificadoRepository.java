f125b4fa145e770d73697dea18133dea6db3443a
Steven Zaldivar <zh15002@ues.edu.sv>
2021-03-05 23:30:32 -0600

    Update CertificadoRepository
    New query added to retrieve one certificate given an student id (carnet)
    and a project name

--- src/main/java/ues/occ/proyeccion/social/ws/app/repository/CertificadoRepository.java
+++ src/main/java/ues/occ/proyeccion/social/ws/app/repository/CertificadoRepository.java
@@ -8,10 +8,14 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import ues.occ.proyeccion.social.ws.app.dao.Certificado;
 
+import java.util.Optional;
+
 @Repository
 public interface CertificadoRepository extends PagingAndSortingRepository<Certificado, Integer> {
 	Page<Certificado> findAllByProyectoEstudiante_Estudiante_Carnet(String carnet, Pageable pageable);
 
     @Query(value = "SELECT p.carnet FROM proyecto_estudiante p where p.id = ?1", nativeQuery = true)
     String findCarnet(int id);
+
+    Optional<Certificado> findByProyectoEstudiante_Estudiante_CarnetAndProyectoEstudiante_Proyecto_NombreContainingIgnoreCase(String carnet, String projectName);
 }
