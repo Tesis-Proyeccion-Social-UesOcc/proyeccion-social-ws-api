283f311dd2befe395df22f03e1cc1fca9df58f06
Steven Zaldivar <zh15002@ues.edu.sv>
2021-04-12 20:43:01 -0600

    Update RequerimientoRepository
    New query added to return only the id values of 'Requerimiento'

--- src/main/java/ues/occ/proyeccion/social/ws/app/repository/RequerimientoRepository.java
+++ src/main/java/ues/occ/proyeccion/social/ws/app/repository/RequerimientoRepository.java
@@ -1,11 +1,17 @@
 package ues.occ.proyeccion.social.ws.app.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 import ues.occ.proyeccion.social.ws.app.dao.Requerimiento;
+import ues.occ.proyeccion.social.ws.app.repository.projections.RequerimientoIdView;
+
+import java.util.List;
 
 @Repository
 public interface RequerimientoRepository extends CrudRepository<Requerimiento, Integer>{
-
+    List<RequerimientoIdView> findAllProjectedBy();
+    @NonNull
+    List<Requerimiento> findAll();
 }
